6a49da993f57efe410ffb6e7f60c484b272868b8
korinnetoliver <korinne.toliver@uri.edu>
2026-02-09 21:07:43 -0500

    Add files via upload

---
+++ lib/tasks/tasks_model.dart
@@ -0,0 +1,28 @@
+import '/auth/firebase_auth/auth_util.dart';
+import '/backend/api_requests/api_calls.dart';
+import '/backend/backend.dart';
+import '/components/add_task_widget.dart';
+import '/components/task_widget.dart';
+import '/flutter_flow/flutter_flow_theme.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+import '/flutter_flow/flutter_flow_widgets.dart';
+import 'dart:ui';
+import '/index.dart';
+import 'tasks_widget.dart' show TasksWidget;
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+
+class TasksModel extends FlutterFlowModel<TasksWidget> {
+  ///  State fields for stateful widgets in this page.
+
+  // Stores action output result for [Backend Call - API (ZenQuote)] action in Button widget.
+  ApiCallResponse? apiResult3pa;
+
+  @override
+  void initState(BuildContext context) {}
+
+  @override
+  void dispose() {}
+}
